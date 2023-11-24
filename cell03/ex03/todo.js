5b226d1bd45047bab78ba1141e8a97283705e1ca
Fiona Selanno <fiselann@shi-r9-p5.s19.be>
2023-11-24 13:40:16 +0100

    todo

--- cell03/ex03/todo.js
+++ cell03/ex03/todo.js
@@ -1,21 +1,52 @@
+let list = document.querySelector("#ft_list");
+
 function askToDo(){
 	let todo = prompt("Please enter your todo-item: ");
-	let list = document.querySelector("#ft_list");
 	console.log(list);
 	if (todo == "")
 		alert("Input can't be empty!");
 	else{
 		let div = document.createElement("div");
+		div.innerHTML=todo;
 		div.className = "item";
-		div.onclick = removeItem();
-		list.prepend(todo, div);
+		list.prepend(div);
 		console.log(list.childNodes);
-
 	}
+	saveDataLocal();
 }
 
-function removeItem(){
-	console.log("remove");
+list.addEventListener("click", (e)=>{
+	let toggle = 0
+	let answer = prompt("Are you sure you want to delete this item? Type in YES or NO:  ");
+	if (e.target.className=="item"){
+		while (toggle == 0){
+			if (answer == "YES")
+			{
+				toggle = 1;
+				e.target.remove();
+				break ;
+			}
+			else if (answer == "NO") 
+			{
+				toggle = 1
+				break;
+			} else {
+				toggle = 0
+				answer = prompt("I don't understsand your answer. Please try again. Type in YES or NO: ")
+			}
+		}	
+	}
+	saveDataLocal();
+});
+
+function saveDataLocal(){
+    localStorage.setItem("data", list.innerHTML);
 }
 
-let item
+function showTask(){
+    const savedData = localStorage.getItem("data");
+    if(savedData){
+        list.innerHTML = savedData;
+    }
+}
+window.addEventListener("load", showTask);
